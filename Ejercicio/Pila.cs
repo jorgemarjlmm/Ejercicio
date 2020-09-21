7f1b0c169ff15d8088d5ddf745f0ac710e4d4006
jorgemarjlmm <71659051+jorgemarjlmm@users.noreply.github.com>
2020-09-21 11:29:43 -0500

    Add files via upload

---
+++ Ejercicio/Pila.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio
+{
+    public class Nodo
+    {
+        private int dato;
+        private Nodo siguiente = null;
+        private int valorMaximoPila;
+
+        public int Dato { get => dato; set => dato = value; }
+        public Nodo Siguiente { get => siguiente; set => siguiente = value; }
+        public int ValorMaximoPila { get => valorMaximoPila; set => valorMaximoPila = value; }
+
+    }
+
+    public class Pila
+    {
+        private Nodo ancla;
+        private Nodo auxAncla;
+
+        public Pila()
+        {
+            ancla = new Nodo();
+            ancla.Siguiente = null;
+        }
+
+        public void Push(int pDato)
+        {
+            int valorMaximo;
+            Nodo ultimoNodo = new Nodo();
+
+            Nodo tmp = new Nodo();
+            tmp.Dato = pDato;
+
+            ultimoNodo = Peek();
+
+            if (ultimoNodo.ValorMaximoPila > pDato)
+            {
+                valorMaximo = ultimoNodo.ValorMaximoPila;
+            }
+            else
+            {
+                valorMaximo = pDato;
+            }
+
+            tmp.ValorMaximoPila = valorMaximo;
+            tmp.Siguiente = ancla.Siguiente;
+
+            ancla.Siguiente = tmp;
+        }
+
+        public Nodo Pop()
+        {
+            Nodo result = new Nodo();
+
+            if (ancla.Siguiente != null)
+            {
+                auxAncla = ancla.Siguiente;
+                result.Dato = auxAncla.Dato;
+                result.ValorMaximoPila = auxAncla.ValorMaximoPila;
+
+                ancla.Siguiente = auxAncla.Siguiente;
+                auxAncla.Siguiente = null;
+            }
+
+            return result;
+        }
+
+        public Nodo Peek()
+        {
+            Nodo result = new Nodo();
+
+            if (ancla.Siguiente != null)
+            {
+                auxAncla = ancla.Siguiente;
+                result.Dato = auxAncla.Dato;
+                result.ValorMaximoPila = auxAncla.ValorMaximoPila;
+            }
+
+            return result;
+        }
+
+        public void Imprimir()
+        {
+            auxAncla = ancla;
+
+            while (auxAncla.Siguiente != null)
+            {
+                auxAncla = auxAncla.Siguiente;
+
+                int datoNodo = auxAncla.Dato;
+                int valorMaximoPila = auxAncla.ValorMaximoPila;
+
+                Console.WriteLine("El número del nodo es: [{0}]", datoNodo);
+                //Console.WriteLine("[{0}]", valorMaximoPila);
+            }
+        }
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila oPila = new Pila();
+            Nodo oUltimoRegistro = new Nodo();
+
+            int opcionMenu;
+            int numero;
+
+            do 
+            {
+                Menu();
+                Console.WriteLine("Tecle la opción del menú que guste realizar;");
+                opcionMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcionMenu)
+                {
+                    case 1:
+                        Console.WriteLine("Tecle el número a guardar en la pila:");
+                        numero = Convert.ToInt32(Console.ReadLine());
+                        oPila.Push(numero);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Se ha eliminado el registro de la pila");
+                        oPila.Pop();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("El último valor en la pila es:");
+                        oUltimoRegistro = oPila.Peek();
+                        Console.WriteLine(oUltimoRegistro.Dato.ToString());
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Los datos de la pila son:");
+                        oPila.Imprimir();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("El valor máximo actual de la pila es:");
+                        oUltimoRegistro = oPila.Peek();
+                        Console.WriteLine(oUltimoRegistro.ValorMaximoPila.ToString());
+                        break;
+                }
+            }
+                while (opcionMenu <= 5) ;
+            
+
+            
+            //oPila.Push(3);
+            //oPila.Push(5);
+            //oPila.Push(7);
+            //oPila.Push(6);
+
+            //oPila.Imprimir();
+
+            //oPila.Pop();
+
+            //oPila.Imprimir();
+
+            //oPila.Peek();
+        }       
+
+        public static void Menu()
+        {            
+            Console.WriteLine("1.- Insertar datos en la pila;");
+            Console.WriteLine("2.- Eliminar datos en la pila");
+            Console.WriteLine("3.- Imprimir el último dato de la pila");
+            Console.WriteLine("4.- Imprimir todos los datos de la pila");
+            Console.WriteLine("5.- Obtener el máximo valor capturado en la pila");
+
+        }
+    }
+}
